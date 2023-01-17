8978067eee53ff74da97ce32546cae8643ecdde5
Tanweer Kamal <tanweerkamal54@gmail.com>
2023-01-17 19:06:21 +0530

    Modified ui.c

--- src/ui.c
+++ src/ui.c
@@ -5,9 +5,9 @@
 void
 ui_home(dhanda *app)
 {
-	int txn = 567,
+	int txn = app->txn_list->count,
 		amount = 12399,
-		party = 56,
+		party = app->party_list->count,
 		debit = 144,
 		credit = 12;
 
@@ -20,7 +20,11 @@ ui_home(dhanda *app)
 	printf("\n");
 	printf("___________     ____________   _____________    ___________     ____________");
 	printf("\n\n");
-	printf("%-15d %-15d %-15d %-15d %-15d ", txn, amount, party, debit, credit);
+	printf("%-15d %-15d %-15d %-15d %-15d ", txn, 
+											amount, 
+											party, 
+											debit, 
+											credit);
 	printf("\n\n");
 	puts("");
 
